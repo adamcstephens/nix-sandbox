076a86679b2623a08dfc13b5e855db5852260b4e
Adam Stephens <adam@valkor.net>
2023-05-05 00:39:47 -0400

    pull in logiops nixos module

--- nixosModules/default.nix
+++ nixosModules/default.nix
@@ -2,6 +2,7 @@
   config.flake.nixosModules = {
     ble2mqtt = import ./ble2mqtt.nix;
     iguanair = import ./iguanair.nix;
+    logiops = import ./logiops;
     mdns-reflector = import ./mdns-reflector.nix;
   };
 }
